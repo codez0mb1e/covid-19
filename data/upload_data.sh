6246f587b4763782e90b55a8b78b09ed5db6cf9b
codez0mb1e <dmitry.petukhov@outlook.com>
2020-04-09 15:59:58 +0000

    Add new datasets and group its in sections

--- data/upload_data.sh
+++ data/upload_data.sh
@@ -1,23 +1,26 @@
 #!/bin/sh
 
 
-echo "Loading COVID Spread Dataset..."
+echo "Loading COVID Spread Datasets..."
 kaggle datasets download -d sudalairajkumar/novel-corona-virus-2019-dataset -p ../data/
 
-echo "Population by Country 2020 Dataset"
-kaggle datasets download -d tanuprabhu/population-by-country-2020  -p ../data/
+wget -P ../data/ https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv
+wget -P ../data/ https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv
+wget -P ../data/ https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv
 
-echo "Loading COVID-19 Global Forecasting Dataset"
+
+echo "Loading Forecasting Datasets..."
 kaggle competitions download -c covid19-global-forecasting-week-3 -p ../data/
-
-echo "Loading Sberbank COVID-19 Global Forecast Dataset"
 wget -P ../data/ https://storage.yandexcloud.net/datasouls-ods/materials/9a76eb73-0965-45b4-bf1d-4920aa317ba1/countries.csv.zip
+
+
+echo "Loading population by Country 2020 Datasets..."
+kaggle datasets download -d tanuprabhu/population-by-country-2020  -p ../data/
+wget -P ../data/ https://raw.githubusercontent.com/vlomme/sberbank-covid19-forecast-2020/master/WorldPopulationByAge2020.csv
+
 
 echo "Loading quarantine_dates"
 wget -P ../data/ https://raw.githubusercontent.com/tyz910/sberbank-covid19/master/data/quarantine_dates.csv
-
-echo "WorldPopulationByAge2020"
-wget -P ../data/ https://raw.githubusercontent.com/vlomme/sberbank-covid19-forecast-2020/master/WorldPopulationByAge2020.csv
 
 
 echo "Completed."
