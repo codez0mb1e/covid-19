7efdda5b1d3cc099d71a6d4940ef92ed4587c6b9
codez0mb1e <dmitry.petukhov@outlook.com>
2020-04-13 14:23:35 +0000

    Add/update datasets

--- data/upload_data.sh
+++ data/upload_data.sh
@@ -7,16 +7,18 @@ kaggle datasets download -p ../data/ -d sudalairajkumar/novel-corona-virus-2019-
 wget -N -P ../data/ https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv
 wget -N -P ../data/ https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv
 wget -N -P ../data/ https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv
+wget -N -P ../data/ https://raw.githubusercontent.com/CSSEGISandData/COVID-19/web-data/data/cases_country.csv
 
 
 echo "Loading Forecasting datasets..."
 kaggle competitions download -p ../data/ -c covid19-global-forecasting-week-4
 wget -N -P ../data/ https://storage.yandexcloud.net/datasouls-ods/materials/9a76eb73-0965-45b4-bf1d-4920aa317ba1/countries.csv.zip
+wget -N -P ../data/ https://storage.yandexcloud.net/datasouls-ods/materials/9a76eb73-0965-45b4-bf1d-4920aa317ba1/sample_submission_dDoEbyO.csv
 
 
 echo "Loading population datasets..."
 kaggle datasets download -p ../data/ -d tanuprabhu/population-by-country-2020
-wget -N -P ../data/ https://raw.githubusercontent.com/vlomme/sberbank-covid19-forecast-2020/master/WorldPopulationByAge2020.csv
+wget -N -P ../data/ https://raw.githubusercontent.com/vlomme/sberbank-covid19-forecast-2020/master/data/WorldPopulationByAge2020.csv
 kaggle datasets download -p ../data/ -d osciiart/smokingstats
 
 
