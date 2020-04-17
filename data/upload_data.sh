b994c923bc5d6cab92b1ca5dd774adee3bb94f7c
codez0mb1e <dmitry.petukhov@outlook.com>
2020-04-17 11:56:47 +0000

    Add new datasets

--- data/upload_data.sh
+++ data/upload_data.sh
@@ -4,6 +4,7 @@
 echo "Loading COVID spread datasets..."
 kaggle datasets download -p ../data/ -d sudalairajkumar/novel-corona-virus-2019-dataset
 kaggle competitions download -p ../data/ -c covid19-global-forecasting-week-4
+kaggle datasets download -p ../data/ -d kapral42/covid19-russia-regions-cases
 
 wget -N -P ../data/ https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv
 wget -N -P ../data/ https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv
@@ -11,15 +12,19 @@ wget -N -P ../data/ https://raw.githubusercontent.com/CSSEGISandData/COVID-19/ma
 wget -N -P ../data/ https://raw.githubusercontent.com/CSSEGISandData/COVID-19/web-data/data/cases_country.csv
 
 
-
-echo "Loading population datasets..."
+echo "Loading socio demographic data..."
+# Populations datasets
 kaggle datasets download -p ../data/ -d tanuprabhu/population-by-country-2020
 wget -N -P ../data/ https://storage.yandexcloud.net/datasouls-ods/materials/9a76eb73-0965-45b4-bf1d-4920aa317ba1/countries.csv.zip
 wget -N -P ../data/ https://raw.githubusercontent.com/vlomme/sberbank-covid19-forecast-2020/master/data/WorldPopulationByAge2020.csv
+kaggle datasets download -p ../data/ -d kwigan/reproduction-rate-russia-regions
 
 
-echo "Google mobility info..."
+echo "Loading mobility info..."
+# Google mobility
 wget -N -P ../data/ https://raw.githubusercontent.com/tyz910/sberbank-covid19/master/data/mobility.csv
+# Apple mobility
+wget -N -P ../data/ https://covid19-static.cdn-apple.com/covid19-mobility-data/2006HotfixDev6/v1/en-us/applemobilitytrends-2020-04-15.csv
 
 
 echo "Loading quarantine info..."
