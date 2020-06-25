dc641da9de853d8eecc73803caa30a75d34e40a1
codez0mb1e <d.petukhov@outlook.com>
2020-06-25 07:06:43 +0000

    Uploader script updates

--- data/tidycovid19-package-data-uploader.R
+++ data/tidycovid19-package-data-uploader.R
@@ -1,4 +1,6 @@
 
+print("Script needs installed tidycovid19 package https://github.com/joachim-gassen/tidycovid19")
+
 data <- tidycovid19::download_merged_data(silent = T, cached = T)
 
 write.table(data, "../data/tidycovid19-package-data.csv",
